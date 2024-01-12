3b076ce10c8a25cd3f95c8f9f536d459e21b7989
Marc Mezger <marc.mezger@gmail.com>
2024-01-12 11:23:57 +0100

    initial.

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,13 +1,18 @@
-from ultra_simple_config import load_config
+"""Simple Test."""
 from omegaconf import DictConf
+
+from ultra_simple_config import load_config
+
 
 @load_config(location="tests/example_config/config.yaml")
 def test_loading(cfg: DictConf):
+    """Test the loading of the configuration file.
 
+    Args:
+        cfg (DictConf): The configuration object.
+    """
     assert cfg.db.url == "localhost"
     assert cfg.db.port == 5432
     assert cfg.db.format == "utf8"
 
     assert cfg.user.name == "blubby"
-
-    
